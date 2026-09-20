8fc2ef3f44a09682bb6a132973735efdf7228c5c
arasteh264 <arasteh264@gmail.com>
2026-09-20 11:35:52 +0330

    fix deploy bug

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,7 @@
-import 'dotenv/config';
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { createApp } from './bootstrap';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://helper-customer.vercel.app'],
-    credentials: true,
-  });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-
-
-  const config = new DocumentBuilder()
-    .setTitle('Helper API')
-    .setDescription('Two-Sided Service Marketplace API')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const app = await createApp();
 
   const port = process.env.PORT ?? 3001;
   await app.listen(port);
